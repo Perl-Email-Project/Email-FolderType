1b0df6568dc27c194993d79df6ba8ae0dbb899ad
Ricardo Signes <rjbs@cpan.org>
2006-08-02 13:37:07 +0000

    r24249@knight:  rjbs | 2006-08-02 09:30:42 -0400  ezmlm fix  r24250@knight:  rjbs | 2006-08-02 09:31:40 -0400  credits  r24251@knight:  rjbs | 2006-08-02 09:32:28 -0400  tag release  r24252@knight:  rjbs | 2006-08-02 09:35:33 -0400  fix prereq, add test

---
+++ lib/Email/FolderType/Maildir.pm
@@ -0,0 +1,14 @@
+package Email::FolderType::Maildir;
+
+=head1 NAME
+
+Email::FolderType::Maildir - class to help Email::FolderType recognise maildirs
+
+=cut
+
+sub match {
+  my $folder = shift;
+  return ($folder =~ m{[^/]/$} || -d "$folder/cur");
+}
+
+1;
